4a4fd23d8bcd36e8a802b332043029b77d13f2e1
intknwn <intknwn@gmail.com>
2020-07-30 12:58:44 +0800

    Исправляет баг с отображением маски в поле ввода

--- source/js/modules/mask.js
+++ source/js/modules/mask.js
@@ -17,6 +17,12 @@ export default () => {
 
     input.addEventListener(`blur`, () => {
       mask.updateOptions({lazy: true});
+
+      const regExp = /\+7 \((\d{3})\) (\d{3})\-(\d{2})\-(\d{2})/;
+
+      if (!mask.value.match(regExp)) {
+        mask.value = ``;
+      }
     });
   });
 };
